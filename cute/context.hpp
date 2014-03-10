c8e153ae098b9a7bdf1a054092a3e1d0bb9662da
Kosta <konstantin.baumann@autodesk.com>
2014-03-10 10:02:08 +0100

    code cleanup

---
+++ cute/context.hpp
@@ -0,0 +1,85 @@
+// Copyright 2014 by Konstantin (Kosta) Baumann
+//
+// Distributed under the MIT license (See accompanying file LICENSE.txt)
+//
+
+#pragma once
+
+#include "exception.hpp"
+#include "macros.hpp"
+#include "test.hpp"
+#include "test_registry.hpp"
+#include "test_result.hpp"
+#include "test_suite_result.hpp"
+
+#include <memory>
+
+namespace cute {
+
+    struct context {
+        std::function<void(test_result const& rep)> reporter;
+        std::string include_tags;
+        std::string exclude_tags;
+
+        inline std::unique_ptr<test_suite_result> run() const {
+            return run(detail::test_registry::instance().tests);
+        }
+
+        inline std::unique_ptr<test_suite_result> run(
+            std::vector<test> const& tests
+        ) const {
+            auto const time_start_all = detail::time_now();
+
+            auto res = std::unique_ptr<test_suite_result>(new test_suite_result());
+
+            auto const incl_tags = detail::parse_tags(include_tags);
+            auto const excl_tags = detail::parse_tags(exclude_tags);
+
+            for(auto&& test : tests) {
+                ++res->test_cases;
+
+                if(detail::skip_test(test.tags, incl_tags, excl_tags)) {
+                    ++res->test_cases_skipped;
+                    continue;
+                }
+
+                auto rep = test_result(test.name, true, "", "", test.file, test.line, 0);
+                auto const time_start = detail::time_now();
+
+                try {
+                    auto const count_start = res->checks_performed.load();
+
+                    --res->checks_performed; // decr by one since CUTE_EXPECT_IMPL() below will increment it again
+                    CUTE_DETAIL_EXPECT(test.file, test.line, ((void)test.test_case(), true), "");
+
+                    auto const count_end = res->checks_performed.load();
+                    if(count_start == count_end) {
+                        throw cute::detail::exception("no check performed in test case", test.file, test.line, "");
+                    }
+
+                    ++res->test_cases_passed;
+                } catch(detail::exception const& ex) {
+                    rep.pass = false;
+                    rep.file = ex.file;
+                    rep.line = ex.line;
+                    rep.msg  = ex.what();
+                    rep.expr = ex.expr;
+
+                    ++res->test_cases_failed;
+                }
+
+                auto const time_end = detail::time_now();
+                rep.duration_ms = detail::time_diff_ms(time_start, time_end);
+
+                if(reporter) { reporter(rep); }
+            }
+
+            auto const time_end_all = detail::time_now();
+            res->duration_ms = detail::time_diff_ms(time_start_all, time_end_all);
+
+            return res;
+        }
+
+    };
+
+} // namespace cute
--- test/main.cpp
+++ test/main.cpp
@@ -3,46 +3,48 @@
 // Distributed under the MIT license (See accompanying file LICENSE.txt)
 //
 
-#include "cute_unittests.hpp"
+#include "../cute/context.hpp"
+#include "../cute/cute.hpp"
 
 CUTE_INIT();
 
 int main(int argc, char* argv[]) {
-    auto&& tests = cute::detail::test_registry::instance().tests;
-    std::cout << "tests registered: " << tests.size() << std::endl;
-
-    auto normal_reporter = [](cute::test_result const& res) {
+    auto pass_ctx = cute::context();
+    pass_ctx.include_tags = "pass";
+    pass_ctx.reporter = [](cute::test_result const& res) {
         cute::command_line_reporter(res.pass ? std::cout : std::cerr, res);
     };
-    auto pass_res = cute::run(tests, normal_reporter, "pass");
+    auto pass_res = pass_ctx.run();
 
-    auto inverse_reporter = [](cute::test_result const& res) {
+    auto fail_ctx = cute::context();
+    fail_ctx.include_tags = "fail";
+    fail_ctx.reporter = [](cute::test_result const& res) {
         auto r = res; r.pass = !r.pass;
         cute::command_line_reporter(r.pass ? std::cout : std::cerr, r);
     };
-    auto fail_res = cute::run(tests, inverse_reporter, "fail");
+    auto fail_res = fail_ctx.run();
 
     // for this unit test in which we check the correct behavior for
     // passing and failing test cases we need to combine both results
     // by inverting the "failed" cases...
-    auto tests_all = pass_res->test_cases.load();
-    auto tests_passed = pass_res->test_cases_passed + fail_res->test_cases_failed;
-    auto tests_failed = pass_res->test_cases_failed + fail_res->test_cases_passed;
-    auto tests_skipped = pass_res->test_cases_skipped + fail_res->test_cases_skipped - tests_all;
-    auto checks_performed = pass_res->checks_performed + fail_res->checks_performed;
-    auto duration_ms = pass_res->duration_ms + fail_res->duration_ms;
+    auto tests_all          = pass_res->test_cases.load();
+    auto tests_passed       = pass_res->test_cases_passed   + fail_res->test_cases_failed;
+    auto tests_failed       = pass_res->test_cases_failed   + fail_res->test_cases_passed;
+    auto tests_skipped      = pass_res->test_cases_skipped  + fail_res->test_cases_skipped - tests_all;
+    auto checks_performed   = pass_res->checks_performed    + fail_res->checks_performed;
+    auto duration_ms        = pass_res->duration_ms         + fail_res->duration_ms;
 
-    auto self_test_failed = ((tests_failed > 0) || (tests_skipped > 0));
+    auto self_test_failed = ((tests_all == 0) || (tests_failed > 0) || (tests_skipped > 0));
 
     auto& out = (self_test_failed ? std::cerr : std::cout);
 
     out << std::endl;
     out << "test summary:" << std::endl;
-    out << tests_passed << " out of " << tests_all << " tests passed." << std::endl;
-    out << tests_failed << " out of " << tests_all << " tests failed." << std::endl;
-    out << tests_skipped << " out of " << tests_all << " tests skipped." << std::endl;
+    out << tests_passed     << " out of " << tests_all << " tests passed." << std::endl;
+    out << tests_failed     << " out of " << tests_all << " tests failed." << std::endl;
+    out << tests_skipped    << " out of " << tests_all << " tests skipped." << std::endl;
     out << checks_performed << " checks performed." << std::endl;
-    out << duration_ms << " ms used for the whole test suite." << std::endl;
+    out << duration_ms      << " ms used for the whole test suite." << std::endl;
 
     return (self_test_failed ? EXIT_FAILURE : EXIT_SUCCESS);
 }
