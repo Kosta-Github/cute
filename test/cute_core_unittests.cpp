a2d5200e865ee52f818b870696f5e08ef5e50ee3
Kosta <konstantin.baumann@autodesk.com>
2014-03-11 12:50:05 +0100

    split unit test file into 2 files: `core` and `cleanup_guard`

--- test/cute_core_unittests.cpp
+++ test/cute_core_unittests.cpp
@@ -4,7 +4,6 @@
 //
 
 #include "../cute/cute.hpp"
-#include "../cute/cleanup_guard.hpp"
 
 CUTE_TEST("CUTE_ASSERT(true) should pass", "pass") {
     CUTE_ASSERT(true);
@@ -66,61 +65,4 @@ CUTE_TEST("CUTE_ASSERT() can capture more than one variable with the help of CUT
     auto str1 = std::string("hello");
     auto str2 = std::string("world");
     CUTE_ASSERT(str1 == str2, CUTE_CAPTURE(str1), CUTE_CAPTURE(str2));
-}
-
-CUTE_TEST("create a cleanup_guard without a callback/cleanup action", "pass") {
-    auto cleanup = cute::cleanup_guard(nullptr);
-    CUTE_ASSERT(true);
-}
-
-CUTE_TEST("create a cleanup_guard and execute the action at scope exit", "pass") {
-    auto value = int(0);
-
-    {
-        auto cleanup = cute::cleanup_guard([&] { value = 42; });
-    }
-
-    CUTE_ASSERT(value == 42);
-}
-
-CUTE_TEST("create a cleanup_guard and execute the action in case of an exception at scope exit", "pass") {
-    auto value = int(0);
-    auto const exception_message = std::string("forced exception");
-
-    try {
-        auto cleanup = cute::cleanup_guard([&] { value = 42; });
-        throw std::runtime_error(exception_message);
-        cleanup.dismiss();
-    } catch(std::runtime_error const& ex) {
-        // ensure that only the expected exception has been thrown
-        CUTE_ASSERT(ex.what() == exception_message);
-    }
-
-    CUTE_ASSERT(value == 42);
-}
-
-CUTE_TEST("create a cleanup_guard and do not execute after calling dismiss()", "pass") {
-    auto value = int(0);
-
-    {
-        auto cleanup = cute::cleanup_guard([&] { value = 42; });
-        cleanup.dismiss();
-    }
-
-    CUTE_ASSERT(value == 0);
-}
-
-CUTE_TEST("create a cleanup_guard and execute the cleanup action explicitly", "pass") {
-    auto value = int(0);
-
-    {
-        auto cleanup = cute::cleanup_guard([&] { value = 42; });
-        CUTE_ASSERT(value == 0);
-        cleanup.cleanup();
-        CUTE_ASSERT(value == 42);
-        value = 0;
-    }
-    
-    // check that the cleanup action was not triggered a second time
-    CUTE_ASSERT(value == 0);
 }
